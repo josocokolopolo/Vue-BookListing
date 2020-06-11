3bc8bb6e192efdccc9782cc2fd435e68ec968a51
Jhon Solier Collazos <swjsolier@gmail.com>
2020-06-10 22:02:37 -0500

    adding the list of books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,22 +1,39 @@
 <template>
-    <div>
-        <h1>{{title}}</h1>
-    </div>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books" :key="book.id">{{book.title}}: {{book.author}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "BookList",
-        data() {
-            return {
-                title: "All Books"
-            }
-        },
-    }
+export default {
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+      books: [
+        { id: 1, title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { id: 2, title: "American Gods", author: "Neil Gaiman" },
+        { id: 3, title: "Amusing Ourselves to Death", author: "Neil Postman" }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>
-h1,h2 {
-    font-weight: normal;
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
